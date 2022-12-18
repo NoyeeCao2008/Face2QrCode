87e47d09051835fe944d045892c25014928442f6
NoyeeCao <noyeecao@outlook.com>
2022-12-19 02:46:21 +0800

    init about

--- app/src/main/java/com/github/noyeecao2008/f2qr/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/github/noyeecao2008/f2qr/ui/notifications/NotificationsFragment.java
@@ -18,14 +18,14 @@ public class NotificationsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+//        NotificationsViewModel notificationsViewModel =
+//                new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textNotifications;
+//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
 
 
