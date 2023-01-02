d1d7a3b90f1ac66c926ff861b0b1f1e4b0400f59
NoyeeCao <noyeecao@outlook.com>
2023-01-03 07:34:07 +0800

    ui

--- app/src/main/java/com/github/noyeecao2008/f2qr/ui/about/AboutFragment.java
+++ app/src/main/java/com/github/noyeecao2008/f2qr/ui/about/AboutFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.github.noyeecao2008.f2qr.databinding.FragmentAboutBinding;
 
@@ -16,14 +18,14 @@ public class AboutFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        NotificationsViewModel notificationsViewModel =
-//                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        AboutViewModel aboutViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
 
         binding = FragmentAboutBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textNotifications;
-//        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.tvVersion;
+        aboutViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
 
 
--- app/src/main/java/com/github/noyeecao2008/f2qr/ui/about/AboutViewModel.java
+++ app/src/main/java/com/github/noyeecao2008/f2qr/ui/about/AboutViewModel.java
@@ -1,19 +1,24 @@
 package com.github.noyeecao2008.f2qr.ui.about;
 
+import android.os.Build;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.github.noyeecao2008.f2qr.BuildConfig;
+
 public class AboutViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> mVersion;
 
     public AboutViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mVersion = new MutableLiveData<>();
+        mVersion.setValue("v" + BuildConfig.VERSION_NAME
+                + "(" + BuildConfig.VERSION_CODE + ") buildOn:" + BuildConfig.BUILD_TIME);
     }
 
     public LiveData<String> getText() {
-        return mText;
+        return mVersion;
     }
 }
